65fb0bde41f8cdeae3f6ee5e3640403d1975936b
Eugene Tan <contact@eugene.io>
2020-10-25 20:34:51 +0800

    fix shellcheck warnings

--- src/scripts/login.sh
+++ src/scripts/login.sh
@@ -4,26 +4,26 @@ login_userpass() {
     username=$1
     password=$2
     path=$3
-    vault login -no-print -method=userpass -path=$path username=$username password=$password
+    vault login -no-print -method=userpass -path="$path" username="$username" password="$password"
 }
 
 login_token() {
     token=$1
-    vault login -no-print -method=token -path=$path token=$token
+    vault login -no-print -method=token -path="$path" token="$token"
 }
 
 login_approle() {
     role_id=$1
     secret_id=$2
     path=$3
-    vault write auth/$path/login role_id=$role_id secret_id=$secret_id | grep token | awk '{print $2}' | head -n 1 > ~/.vault-token
+    vault write "auth/$path/login" role_id="$role_id" secret_id="$secret_id" | grep token | awk '{print $2}' | head -n 1 > ~/.vault-token
 }
 
 login_main() {
     METHOD=$1
     AUTH_PATH="$PARAM_PATH"
 
-    if [ "$AUTH_PATH" == "" ]; then
+    if [ "$AUTH_PATH" = "" ]; then
         AUTH_PATH=$METHOD
     fi
 
